883d11904f50cc00f6982f247f5ab61576adf028
Jakob Larsson <jakob.larsson7@learnet.se>
2021-11-06 10:16:44 +0100

    Added sass

--- front-page.php
+++ front-page.php
@@ -9,10 +9,22 @@ $pageImageArray = get_field('page_img');
 $pageImage = $pageImageArray['sizes']['large'];
 ?>
 
-<div class="first-view">
-    <div class="main-text">
-    <p data-text="PSYCHO">PSYCHO</p>
-    </div>
+<div id="content-frontpage">
+	<div id="wrapper-frontpage">
+		<!-- Make sure data-text equals the text you put inside the tags. -->
+        <div class="text-area">
+        <p class="sub-bigger">HELLO I AM JAKOB LARSSON FROM SWEDEN WHO IS STUDYING  </p>
+        </div>
+        <div class="headline-area">
+        <h1 class="glitch " data-text="
+            Web Developement">Web Developement</h1>
+        </div>
+		<div class="end-area">
+        <span class="sub">EFFECT</span>
+        </div>
+		
+        <button>Click me</button>
+	</div>
 </div>
 
 
