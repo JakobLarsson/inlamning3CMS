9a167fe33791c90f190140b5cf54c76c8151c4fa
Jakob Larsson <jakob.larsson7@learnet.se>
2021-11-11 16:41:11 +0100

    start pa skol sida

--- page-school.php
+++ page-school.php
@@ -5,23 +5,35 @@ get_header();
 $big = get_field('big-label');
 $small1 = get_field('second-label');
 $small2 = get_field('info-text');
+$courses = get_field('info-courses');
 $pageImageArray = get_field('img');
 $pageImage = $pageImageArray['sizes']['large'];
 ?>
 
-<div id="content-frontpage">
-	<div id="wrapper-frontpage">
+<div id="content-school">
+	<div class="container">
+        <div class="row">
+            <div class="col-7">
+             <div class="img">
+              <img class="img-fpage" src="<?php echo $pageImage ?>" alt="">
+           </div>
+           <div class="headline-area">
+             <h1 class="glitch " data-text="
+             <?php echo $big ?>"><?php echo $big ?></h1>
+          </div>
+          <div class="end-area">
+              <span class="sub"><?php echo $small2 ?></span>
+          </div>
+        </div>
+        <div class="col-4" >
+             <div class="end-area">
+                 <span class="sub"><?php echo $courses ?></span>
+             </div>
+        </div>
+    </div>
 		<!-- Make sure data-text equals the text you put inside the tags. -->
-        <div class="img">
-            <img class="img-fpage" src="<?php echo $pageImage ?>" alt="">
-        </div>
-        <div class="headline-area">
-        <h1 class="glitch " data-text="
-        <?php echo $big ?>"><?php echo $big ?></h1>
-        </div>
-		<div class="end-area">
-        <span class="sub"><?php echo $small2 ?></span>
-        </div>
+        
+		
 
 		
         <button>Click me</button>
