e90bceb33ef64ae620c7faedd3b2c5d5df933a37
Jakob Larsson <jakob.larsson7@learnet.se>
2021-11-14 17:48:15 +0100

    Sista commit1

--- front-page.php
+++ front-page.php
@@ -12,12 +12,13 @@ $pageImage = $pageImageArray['sizes']['large'];
 <div id="content-frontpage">
 	<div id="wrapper-frontpage">
         <div class="container">
-        <div class="img">
-            <img class="img-fpage" src="<?php echo $pageImage ?>" alt="">
-        </div>
+        
         <div class="headline-area">
         <h1 class="glitch " data-text="
         <?php echo $big ?>"><?php echo $big ?></h1>
+        </div>
+        <div class="img mb-3 mt-3">
+            <img class="img-fpage" src="<?php echo $pageImage ?>" alt="">
         </div>
 		<div class="end-area">
         <span class="sub"><?php echo $small2 ?></span>
